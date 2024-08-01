385729eec1290fdf7717a6aa1bddde97b6bf70dd
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-08-01 12:02:18 -0700

    reversing string lab

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let output=''
+  for (let i = str.length -1; i> -1; --i){
+    output = output +str[i]
+  }
+  return output
 }
 
 if (require.main === module) {
@@ -16,4 +21,9 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// orig_s='dsf'
+// output=''
+// for loop if i < origin_s.length() {
+// output + string character
+// }
 // And a written explanation of your solution
